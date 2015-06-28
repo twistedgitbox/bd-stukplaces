dbd4bba78004cf8cb856603925fbc08175f50b8c
twistedgitbox <jcisecurebuildinfo@yahoo.com>
2015-06-28 15:49:57 -0700

    Added review to places

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
-  has_many :places
+  has_many :places, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
 end
