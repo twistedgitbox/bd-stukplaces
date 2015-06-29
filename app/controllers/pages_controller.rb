8f56c0d136ab1cf291a0f82697d091f917a7892e
twistedgitbox <jcisecurebuildinfo@yahoo.com>
2015-06-28 22:13:06 -0700

    Created a dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!
+
   def home
+  	if current_user
+  		redirect_to dashboard_path
+  	end
+
   end
+
+  def dashboard
+  	@user = current_user
+  	@places = @user.places
+  	@reviews = @user.reviews
+  end
+
 end
