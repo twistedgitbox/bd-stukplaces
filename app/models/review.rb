1e88fde4288d87aa4408e392f3f83db0d25d4854
twistedgitbox <jcisecurebuildinfo@yahoo.com>
2015-06-28 19:55:39 -0700

    Added score to reviews using raty

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+
+	validates_presence_of :score
+	validates_presence_of :content
 	
 end
---
+++ db/migrate/20150629022009_add_score_to_reviews.rb
@@ -0,0 +1,6 @@
+class AddScoreToReviews < ActiveRecord::Migration
+  def change
+    add_column :reviews, :score, :integer, default: 0
+    
+  end
+end
