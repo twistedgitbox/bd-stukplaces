0ae624e8eb43642d20604d8e0e357b852e62b0c5
twistedgitbox <jcisecurebuildinfo@yahoo.com>
2015-06-28 22:56:29 -0700

    Made landing page beautiful

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,13 @@
 class PagesController < ApplicationController
 	before_action :authenticate_user!
 
-  def home
-  	if current_user
-  		redirect_to dashboard_path
-  	end
+	def home
+	end
 
-  end
-
-  def dashboard
-  	@user = current_user
-  	@places = @user.places
-  	@reviews = @user.reviews
-  end
+	def dashboard
+		@user = current_user
+		@places = @user.places
+		@reviews = @user.reviews
+	end
 
 end
